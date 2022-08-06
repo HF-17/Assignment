af396e7d9f84f9e1a49a8c4616126f370c13959b
Halil Fidan <halilfidan131@gmail.com>
2022-08-06 18:35:31 +0300

    changed

--- tasks/KDV/Main.java
+++ tasks/KDV/Main.java
@@ -1,7 +1,32 @@
 package tasks.KDV;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi!");
+
+        double bill,kdv,aKdv,rKdv=0.18;
+        String kdv_rate;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your bill: ");
+        bill = input.nextDouble();
+
+        rKdv = (bill>0 && bill<1000) ? 0.18 : 0.08;
+        kdv_rate = (bill>0 && bill<1000) ? "%18" : "%8";
+
+        aKdv = bill * rKdv;
+        kdv = bill + aKdv;
+
+        System.out.println("Bill(not with KDV): " + bill);
+        System.out.println("Rate of KDV: " + kdv_rate);
+        System.out.println("Bill with KDV: " + kdv);
+        System.out.println("Bill of KDV: " + aKdv);
+
+
+
+
+
     }
 }
