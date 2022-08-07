71d17e13ef521525deb192e28dfe7e3c87d6b6ee
Halil Fidan <halilfidan131@gmail.com>
2022-08-07 12:50:38 +0300

    added

---
+++ tasks/Hypotenuse_Calculator/Main.java
@@ -0,0 +1,29 @@
+package tasks.Hypotenuse_Calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        double c,u,area;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter first side of triangle: ");
+        a = input.nextInt();
+        System.out.println("Please enter second side of triangle: ");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+        u = (a + b + c)/2;
+        area = (a*b)/2.0;
+        System.out.println("Hypotenuse is: " + c);
+        System.out.println("Area is: " + area);
+        System.out.println("Perimeter is: " + 2*u);
+
+
+
+
+
+    }
+}
