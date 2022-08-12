d3415dd8cafdb1d7be4bd51864c26bf26a838512
Halil Fidan <halilfidan131@gmail.com>
2022-08-12 15:17:54 +0300

    added

--- tasks/Chinese_Zodiac/Main.java
+++ tasks/Chinese_Zodiac/Main.java
@@ -18,27 +18,27 @@ public class Main {
             case 0:
                 System.out.println("Your Chinese zodiac is MONKEY" );break;
             case 1:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is COCKEREL" );break;
             case 2:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is DOG" );break;
             case 3:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is PIG" );break;
             case 4:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is MOUSE" );break;
             case 5:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is OX" );break;
             case 6:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is TIGER" );break;
             case 7:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is RABBIT" );break;
             case 8:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is DRAGON" );break;
             case 9:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is SNAKE" );break;
             case 10:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is HORSE" );break;
             case 11:
-                System.out.println("Your Chinese zodiac is MONKEY" );break;
+                System.out.println("Your Chinese zodiac is SHEEP" );break;
 
         }
 
