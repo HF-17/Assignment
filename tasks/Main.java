1f6e8b86b0747cafb3e974062e26dc89524eebcb
Halil Fidan <halilfidan131@gmail.com>
2022-08-15 15:20:34 +0300

    added

--- tasks/Main.java
+++ tasks/Main.java
@@ -1,9 +1,26 @@
 package tasks;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int n;
+        double m;
+        String s;
+
+        Scanner input = new Scanner(System.in);
+
+        s = input.nextLine();
+        n = input.nextInt();
+        m = input.nextDouble();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + m);
+        System.out.println("Int: " + n);
+
+
     }
 }
