f79896f9b9ba7c8f111b66f2ba62cd5ee51062fc
Halil Fidan <halilfidan131@gmail.com>
2022-08-15 16:41:45 +0300

    added

---
+++ tasks/Prime_Number/Main.java
@@ -0,0 +1,28 @@
+package tasks.Prime_Number;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int temp=3,flag=0,m;
+
+
+        while (temp<100) {
+
+            m = temp/2 + 1;
+
+            for (int i=2; i<=m; i++) {
+                if (temp % i == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(temp);
+            }
+            flag=0;
+
+            temp++;
+        }
+    }
+}
